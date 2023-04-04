9f19e1a733cdb2ed4a297e35314cb369d8125210
adrian salona <Adrian>
2023-04-04 20:42:20 +0200

    Feat: Basic structure of the user model generated

---
+++ pruebaSymfony/src/Entity/User.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Usuario
+{
+    const CATEGORIA_X = 'X';
+    const CATEGORIA_Y = 'Y';
+    const CATEGORIA_Z = 'Z';
+
+    private $id;
+
+    private $nombre;
+
+    private $apellidos;
+
+    private $poblacion;
+
+    private $categoria;
+
+    private $edad;
+
+    private $activo;
+
+    private $fecha_creacion;
+
+    private $fecha_modificacion;
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getPoblacion(): ?string
+    {
+        return $this->poblacion;
+    }
+
+    public function setPoblacion(string $poblacion): self
+    {
+        $this->poblacion = $poblacion;
+
+        return $this;
+    }
+
+    public function getCategoria(): ?string
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(string $categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    public function isActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    public function setActivo(bool $activo): self
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_creacion;
+    }
+
+    public function setFechaCreacion(\DateTimeInterface $fecha_creacion): self
+    {
+        $this->fecha_creacion = $fecha_creacion;
+
+        return $this;
+    }
+
+    public function getFechaModificacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_modificacion;
+    }
+
+    public function setFechaModificacion(\DateTimeInterface $fecha_modificacion): self
+    {
+        $this->fecha_modificacion = $fecha_modificacion;
+
+        return $this;
+    }
+}
