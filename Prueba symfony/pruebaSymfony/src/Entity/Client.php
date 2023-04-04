ec442d4549a3539f96427e62c14daaa77f74d0f8
adrian salona <Adrian>
2023-04-04 20:15:59 +0200

    Basic project structure and customer model are added.

---
+++ Prueba symfony/pruebaSymfony/src/Entity/Client.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Cliente
+{
+    
+    private $id;
+
+    private $nombre;
+
+    private $direccion;
+
+    private $email;
+
+    private $telefono;
+
+    private $fechaCreacion;
+
+    private $fechaModificacion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): self
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fechaCreacion;
+    }
+
+    public function setFechaCreacion(\DateTimeInterface $fechaCreacion): self
+    {
+        $this->fechaCreacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    public function getFechaModificacion(): ?\DateTimeInterface
+    {
+        return $this->fechaModificacion;
+    }
+
+    public function setFechaModificacion(\DateTimeInterface $fechaModificacion): self
+    {
+        $this->fechaModificacion = $fechaModificacion;
+
+        return $this;
+    }
+}
